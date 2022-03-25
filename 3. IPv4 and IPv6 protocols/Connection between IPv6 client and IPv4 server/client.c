caf7c8a3ce5e495b325b13f83330b11859099416
lukaszgrudnik <baltazarkacper515@gmail.com>
2022-03-25 10:32:19 +0100

    Connection between IPv6 client and IPv4 server implemented

--- 3. IPv4 and IPv6 protocols/Connection between IPv6 client and IPv4 server/client.c
+++ 3. IPv4 and IPv6 protocols/Connection between IPv6 client and IPv4 server/client.c
@@ -7,14 +7,28 @@
 #include <stdlib.h>
 #include <arpa/inet.h>
 #include <net/if.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
 
 #define handle_error(msg) \
            do { perror(msg); exit(EXIT_FAILURE); } while (0)
 
 int main(int argc, char *argv[]) {
 
-  int sfd = socket(AF_INET6, SOCK_STREAM, 0);
 
+  if(argc < 2 || argc > 4){
+    printf("%s\n", "error: invalid arguments\nType client6 --help to get more information.");
+    exit(EXIT_FAILURE);
+  }
+
+  if(argc == 2 && !strcmp(argv[1], "--help")){
+    printf("%s\n", "To run this program type: client6 <address IPv6> <port> <interface>");
+    exit(0);
+  }
+
+
+  int sfd = socket(AF_INET6, SOCK_STREAM, 0);
 
   struct sockaddr_in6 addr;
 
@@ -28,6 +42,15 @@ int main(int argc, char *argv[]) {
   addrlen = sizeof(addr);
 
   connect(sfd, (struct sockaddr *) &addr, addrlen);
+
+  printf("%s\n", "Connection established.");
+
+  char buffer[256];
+  int rrv = recv(sfd, buffer, 256, 0);
+
+  printf("Recevied message from server: %s\n", buffer );
+
+  close(sfd);
 
 
 }
--- 3. IPv4 and IPv6 protocols/Connection between IPv6 client and IPv4 server/server.c
+++ 3. IPv4 and IPv6 protocols/Connection between IPv6 client and IPv4 server/server.c
@@ -5,47 +5,76 @@
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
 
 #define handle_error(msg) \
            do { perror(msg); exit(EXIT_FAILURE); } while (0)
 
-int main() {
+int main(int argc, char *argv[]) {
+
+
+  if(argc < 2 || argc > 3){
+    printf("%s\n", "error: invalid arguments\nType server4 --help to get more information.");
+    exit(EXIT_FAILURE);
+  }
+
+  if(argc == 2 && !strcmp(argv[1], "--help")){
+    printf("%s\n", "To run this program type: server4 <port>");
+    exit(0);
+  }
+
 
   int sfd = socket(AF_INET, SOCK_STREAM, 0);
 
   if(sfd == -1)
       handle_error(strerror(errno));
 
-  struct sockaddr_in addr;
+  struct sockaddr_in server_addr_in, client_addr_in;
 
-  // Reset address structe:
-  memset(&addr, 0, sizeof(addr));
+  memset(&server_addr_in, 0, sizeof(server_addr_in));
 
-  addr.sin_family = AF_INET;
-  addr.sin_port = htons(8080);  // port in network byte order
-  addr.sin_addr.s_addr = INADDR_ANY; // address in network byte order
+  server_addr_in.sin_family = AF_INET;
+  server_addr_in.sin_port = htons(atoi(argv[1]));  // port in network byte order
+  server_addr_in.sin_addr.s_addr = INADDR_ANY; // address in network byte order
 
-  socklen_t addrlen;
-  addrlen = sizeof(addr);
+  socklen_t server_addrlen, client_addrlen;
 
-  if(bind(sfd, (struct sockaddr *) &addr, addrlen) == -1)
+  server_addrlen = sizeof(server_addr_in);
+  client_addrlen = sizeof(client_addr_in);
+
+  if(bind(sfd, (struct sockaddr *) &server_addr_in, server_addrlen) == -1)
       handle_error(strerror(errno));
 
 
   if(listen(sfd, 50) == -1)
     handle_error(strerror(errno));
 
+  char buffer[256];
+  char address[INET_ADDRSTRLEN];
+
+  strcpy(buffer, "Hello World");
+
+
 
   do {
 
-    int arv = accept(sfd, NULL, NULL);
+    int arv = accept(sfd, (struct sockaddr *) &client_addr_in, &client_addrlen);
 
+    if(arv == -1)
+      break;
 
+    inet_ntop(AF_INET, &client_addr_in, address, INET_ADDRSTRLEN);
 
-    if(arv > 0)
-      printf("%s\n","new connection" );
+    printf("Client:\n\tIPv4 address: %s\n\tport: %d\n", address, client_addr_in.sin_port);
+
+    write(arv, buffer, strlen(buffer));
+
+    close(arv);
 
   } while(1);
 
+  close(sfd);
 
 }
